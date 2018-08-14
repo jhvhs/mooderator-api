cbfcf69a1595d40966272167c312f2425bd23a0e
Fabio Mangione <fabio.mangione@gmail.com>
2018-08-14 17:18:07 +0100

    Initial commit

---
+++ src/main/java/io/pivotal/mooderator/MooderatorApiApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.mooderator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooderatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooderatorApiApplication.class, args);
+	}
+}
