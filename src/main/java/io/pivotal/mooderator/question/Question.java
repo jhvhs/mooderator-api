c32131c3e955bee01491b54e279081a585638e00
Fabio Mangione <fabio.mangione@gmail.com>
2018-08-17 22:02:11 +0100

    Add question id

--- src/main/java/io/pivotal/mooderator/question/Question.java
+++ src/main/java/io/pivotal/mooderator/question/Question.java
@@ -9,5 +9,6 @@ import lombok.Setter;
 @Setter
 public class Question {
 
+    private Long id;
     private String sentence;
 }
--- src/test/java/io/pivotal/mooderator/integration/QuestionsTests.java
+++ src/test/java/io/pivotal/mooderator/integration/QuestionsTests.java
@@ -1,13 +1,10 @@
 package io.pivotal.mooderator.integration;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +12,7 @@ import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -30,6 +28,7 @@ public class QuestionsTests {
         ResponseEntity<Map> response = restTemplate.getForEntity("/questions/latest", Map.class);
 
         assertThat(response.getStatusCode(), is(equalTo(OK)));
+        assertThat(response.getBody().get("id"), is(notNullValue()));
         assertThat(response.getBody().get("sentence"), is(equalTo("is this a question?")));
     }
 }
