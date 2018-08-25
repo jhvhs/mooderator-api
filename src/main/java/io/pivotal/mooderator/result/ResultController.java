e09f3f83a1baaeee44e41d0f8ef2eb2bae272d1b
Fabio Mangione <fabio.mangione@gmail.com>
2018-08-25 16:03:33 +0100

    Add endpoint for returning all saved results

--- src/main/java/io/pivotal/mooderator/result/ResultController.java
+++ src/main/java/io/pivotal/mooderator/result/ResultController.java
@@ -1,11 +1,22 @@
 package io.pivotal.mooderator.result;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
+@RequestMapping("/results")
 @RestController
 public class ResultController {
 
@@ -16,9 +27,22 @@ public class ResultController {
     }
 
     @ResponseStatus(CREATED)
-    @PostMapping("/results")
+    @PostMapping
     @ResponseBody
     public Result saveResult(@Valid @RequestBody Result result) {
         return resultRepository.save(result);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<Result> getResults() {
+        return resultRepository.findAll();
+    }
+
+    @Profile("test")
+    @ResponseStatus(NO_CONTENT)
+    @DeleteMapping
+    public void deleteResults() {
+        resultRepository.deleteAll();
     }
 }
--- src/test/groovy/io/pivotal/mooderator/integration/ResultsTests.groovy
+++ src/test/groovy/io/pivotal/mooderator/integration/ResultsTests.groovy
@@ -3,9 +3,11 @@ package io.pivotal.mooderator.integration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.ResponseEntity
 import spock.lang.Specification
 
 import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.OK
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ResultsTests extends Specification {
@@ -13,16 +15,19 @@ class ResultsTests extends Specification {
     @Autowired
     private TestRestTemplate restTemplate
 
-    def "Should save a result"() {
-        given:
-        def requestBody = [
-                questionId: 1L,
-                question  : "a question",
-                answer    : "yes"
-        ]
+    def requestBody = [
+            questionId: 1L,
+            question  : "a question",
+            answer    : "yes"
+    ]
 
+    void cleanup() {
+        restTemplate.delete("/results")
+    }
+
+    def "Should save a result"() {
         when:
-        def response = restTemplate.postForEntity("/results", requestBody, Map.class)
+        def response = postResult()
 
         then:
         response.statusCode == CREATED
@@ -30,5 +35,28 @@ class ResultsTests extends Specification {
         response.body.questionId == requestBody.questionId
         response.body.question == requestBody.question
         response.body.answer == requestBody.answer
+    }
+
+    def "Should return saved results"() {
+        when:
+        def response = postResult()
+
+        then:
+        response.statusCode == CREATED
+
+        when:
+        response = restTemplate.getForEntity("/results", List.class)
+
+        then:
+        response.statusCode == OK
+        response.body.size() == 1
+        response.body[0].id != null
+        response.body[0].questionId == requestBody.questionId
+        response.body[0].question == requestBody.question
+        response.body[0].answer == requestBody.answer
+    }
+
+    private ResponseEntity<Map> postResult() {
+        restTemplate.postForEntity("/results", requestBody, Map.class)
     }
 }
