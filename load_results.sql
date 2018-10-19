61fefa16890e856c871533b1df799c27940224ed
Fabio Mangione <fabio.mangione@gmail.com>
2018-10-19 14:54:56 +0100

    Query for grouping results by date
    - cf commands for db

--- load_results.sql
+++ load_results.sql
@@ -1,1 +1,5 @@
+-- results by answer
 select question, question_id, answer, count(*) as count from result where question_id = (select id from question order by id limit 1) group by answer;
+
+-- results by answer and date
+select question, question_id, answer_id, answer, date(sent_date) as dat, count(*) as count from result where question_id = (select id from question order by id limit 1) group by answer, dat order by dat;
