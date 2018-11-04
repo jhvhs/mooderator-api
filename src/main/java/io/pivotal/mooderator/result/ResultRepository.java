5d330b526a2e9ecf5ac99d4af93d23a85b6e3d1d
Fabio Mangione <fabio.mangione@gmail.com>
2018-11-04 23:38:47 +0000

    Fix integration tests
    - added embedded MySql for running integration tests

--- src/main/java/io/pivotal/mooderator/result/ResultRepository.java
+++ src/main/java/io/pivotal/mooderator/result/ResultRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Long> {
-    @Query(value = "select new io.pivotal.mooderator.result.SurveyAnswerStatistics(r.question, r.questionId, r.answer, COUNT(r) AS results) from Result r where r.questionId = ?1 group by r.answer")
+    @Query(value = "select new io.pivotal.mooderator.result.SurveyAnswerStatistics(r.question, r.questionId, r.answer, COUNT(r) AS results) from Result r where r.questionId = ?1 group by r.question, r.answer")
     List<SurveyAnswerStatistics> findSurveyStatistics(Long questionId);
 
     @Query(value = "select new io.pivotal.mooderator.result.SurveyAnswerStatistics(r.question, r.questionId, r.answer, count(r) as results, date(r.sentDate) as day) from Result r where r.questionId = ?1 group by r.answer, date(r.sentDate) order by day desc")
