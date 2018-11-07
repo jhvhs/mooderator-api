8f8aa0fcddbea4090765cf9d2b7c2f1012d45fc2
Fabio Mangione <fmangione@Fabios-MacBook-Pro.local>
2018-11-07 11:41:55 +0000

    Change the order of stats to be ascending

--- src/main/java/io/pivotal/mooderator/result/ResultRepository.java
+++ src/main/java/io/pivotal/mooderator/result/ResultRepository.java
@@ -11,6 +11,6 @@ public interface ResultRepository extends JpaRepository<Result, Long> {
     @Query(value = "select new io.pivotal.mooderator.result.SurveyAnswerStatistics(r.question, r.questionId, r.answer, COUNT(r) AS results) from Result r where r.questionId = ?1 group by r.question, r.answer")
     List<SurveyAnswerStatistics> findSurveyStatistics(Long questionId);
 
-    @Query(value = "select new io.pivotal.mooderator.result.SurveyAnswerStatistics(r.question, r.questionId, r.answer, count(r) as results, date(r.sentDate) as day) from Result r where r.questionId = ?1 group by r.answer, date(r.sentDate) order by day desc")
+    @Query(value = "select new io.pivotal.mooderator.result.SurveyAnswerStatistics(r.question, r.questionId, r.answer, count(r) as results, date(r.sentDate) as day) from Result r where r.questionId = ?1 group by r.answer, date(r.sentDate) order by day asc")
     List<SurveyAnswerStatistics> findSurveyStatisticsPerDay(Long questionId);
 }
