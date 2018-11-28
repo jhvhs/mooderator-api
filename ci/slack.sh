defe6b26ae1c701e9f2b54c861bad44647fa2db8
Fabio Mangione <fabio.mangione@gmail.com>
2018-11-28 23:29:52 +0000

    Send daily notifications

--- ci/slack.sh
+++ ci/slack.sh
@@ -2,25 +2,38 @@
 
 set -euox pipefail
 
-load_questions() {
-    rep=$(curl 'http://mooderator-api.cfapps.io/questions/latest' -H 'Content-Type: application/json' | jq -r '.id')
-    echo ${rep}
-}
-
 load_stats() {
     stats=$(curl 'http://mooderator-api.cfapps.io/results/statistics' -H 'Content-Type: application/json')
     echo ${stats}
 }
 
+load_daily_stats() {
+    stats=$(curl 'http://mooderator-api.cfapps.io/results/daily-statistics' -H 'Content-Type: application/json')
+    echo ${stats}
+}
+
 main() {
-#    questionId=$(load_questions)
-    payload=$(load_stats)
+    #TODO day should consider if today is Monday
+    day=$(date -v-1d +%F)
+    echo ${day}
+
+    payload=$(load_daily_stats)
 
     question=$(echo ${payload} | jq '.[0] | .question')
 
-    msg=$(echo ${payload} | jq 'map("\(.answer) \(.results)") | join(" - ")' )
+    filter="map(select(.day == \"${day}\")) | map(\"\(.answer) \(.results)\") | join(\" - \")"
 
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\": ${question}, \"attachments\" : [{\"text\" : ${msg}}] }" https://hooks.slack.com/services/$WEBHOOK_SUFFIX
+    msg=$(echo ${payload} | jq "${filter}")
+
+#TODO fix this if
+#    if [ $msg != '""' ]
+#    then
+        msg="\"${day} : ${msg//\"}\""
+        curl -X POST -H 'Content-type: application/json' --data "{\"text\": ${question}, \"attachments\" : [{\"text\" : ${msg}}] }" https://hooks.slack.com/services/$WEBHOOK_SUFFIX
+        echo "Notification sent"
+#    else
+#        echo "Nothing to notify"
+#    fi
 }
 
 main
